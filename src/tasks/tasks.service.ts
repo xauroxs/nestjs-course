79408724e51c5ddbf19542c4c936837f06550cca
xauroxs <xauroxs@gmail.com>
2023-09-19 23:44:49 +0300

    create createTask method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
-import { Task } from './task.model';
+import { TASK_STATUSES, Task } from './task.model';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
+  private id: number = 0;
 
   getAllTasks() {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string) {
+    const newTask: Task = {
+      id: `${this.id++}`,
+      title,
+      description,
+      status: TASK_STATUSES.OPEN,
+    };
+
+    this.tasks.push(newTask);
+
+    return newTask;
   }
 }
