1178fceac938106c44e0018726d57e04466e3227
xauroxs <xauroxs@gmail.com>
2023-09-20 22:23:45 +0300

    apply validation to filtering tasks

--- src/tasks/dto/filter-tasks.dto.ts
+++ src/tasks/dto/filter-tasks.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TASK_STATUSES } from '../task.model';
 
 export class FilterTasksDto {
+  @IsOptional()
+  @IsEnum(TASK_STATUSES)
   status?: TASK_STATUSES;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
