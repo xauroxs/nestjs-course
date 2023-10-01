e048344956fbc92f90100003381ba45bae0e176d
xauroxs <xauroxs@gmail.com>
2023-10-01 21:49:03 +0300

    create signUp and createUser methods for auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('sign-up')
+  signUp(@Body() dto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(dto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,27 @@ import { Repository } from 'typeorm';
 
 import { User } from './user.entity';
 
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
+
+  private async createUser(dto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = dto;
+
+    const user = this.usersRepository.create({
+      username,
+      password,
+    });
+
+    await this.usersRepository.save(user);
+  }
+
+  async signUp(dto: AuthCredentialsDto): Promise<void> {
+    return this.createUser(dto);
+  }
 }
